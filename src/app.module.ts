b1f597ec6efb70614d1e714dd2e6a9edb403194d
marianfer cruz <marianfercruz1512@gmail.com>
2025-11-14 10:29:43 -0400

    [feat]: Implement authentication and user management modules with controllers, services, and guards

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthController } from './auth/controllers/auth.controller';
+import { AuthService } from './auth/services/auth.service';
+import { UserController } from './users/controllers/user.controller';
+import { UserService } from './users/services/user.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, AuthController, UserController],
+  providers: [AppService, AuthService, UserService],
 })
 export class AppModule {}
