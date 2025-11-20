c38abd2ad948ff2ffdb5fe5c713082ed00f90cb6
marianfer cruz <marianfercruz1512@gmail.com>
2025-11-20 15:38:12 -0400

    [feat]: Add UserModule and AuthModule to the application

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,11 @@ import { AuthController } from './auth/controllers/auth.controller';
 import { AuthService } from './auth/services/auth.service';
 import { UserController } from './users/controllers/user.controller';
 import { UserService } from './users/services/user.service';
+import { UserModule } from './users/user.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, AuthModule],
   controllers: [AppController, AuthController, UserController],
   providers: [AppService, AuthService, UserService],
 })
